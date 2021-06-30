6125d3a5d1e4ddf073e895edbe2efcb27510ed7a
Kronu97 <mihai.soml3a@gmail.com>
2021-06-30 22:05:31 +0300

    improved the directive

--- practical-project/src/app/shared/dropdown.directive.ts
+++ practical-project/src/app/shared/dropdown.directive.ts
@@ -12,11 +12,11 @@ import {
 export class DropdownDirective implements OnInit {
     @HostBinding('class.open') isOpen = false;
 
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
 
-    constructor() {}
+    constructor(private elRef: ElementRef) {}
 
     // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
     ngOnInit(): void {}
