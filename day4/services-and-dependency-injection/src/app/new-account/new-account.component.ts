84706086ef354168f29aee019431c9ac6b5d3913
Kronu97 <mihai.soml3a@gmail.com>
2021-07-02 20:38:59 +0300

    added a new account service

--- day4/services-and-dependency-injection/src/app/new-account/new-account.component.ts
+++ day4/services-and-dependency-injection/src/app/new-account/new-account.component.ts
@@ -1,22 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../accounts.service';
 
 import { LoggingService } from '../logging.service';
 @Component({
-  selector: 'app-new-account',
-  templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+    selector: 'app-new-account',
+    templateUrl: './new-account.component.html',
+    styleUrls: ['./new-account.component.css'],
+    providers: [LoggingService, AccountsService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+    constructor(
+        private loggingService: LoggingService,
+        private accountService: AccountsService
+    ) {}
 
-  constructor(private loggingService: LoggingService) {}
-
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    this.loggingService.logStatusChange(accountStatus);
-  }
+    onCreateAccount(accountName: string, accountStatus: string) {
+        this.accountService.addAccount(accountName, accountStatus);
+        this.loggingService.logStatusChange(accountStatus);
+    }
 }
